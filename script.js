22f761a0ac7b47f2428cbdd0811ec5536b6ab124
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-04-16 21:23:52 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.onload = function() {
   var title = document.getElementById("username");
   title.addEventListener("mousemove", function(e) {
+    // mouse_y is the y-value within the title element; ranges from 0 (top) to height of title (left)
+    // mouse_x is the x-value within the title element; ranges from 0 (left) to width of title (right)
     var mouse_y = e.clientY;
     var mouse_x = e.clientX;
-    title.style.paddingTop = String(mouse_y) + "px";
-    title.style.paddingLeft = String(mouse_y) + "px";
+    // example code: sets padding to the x and y position of mouse, pushing the title out and down the page.
+     title.style.paddingTop = String(mouse_y) + "px";
+     title.style.paddingLeft = String(mouse_x) + "px";
   });
 };
