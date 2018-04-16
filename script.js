f00dfaeee85f3fd461f57abf8aa24609c607e2fc
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-04-16 21:24:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function() {
     // mouse_x is the x-value within the title element; ranges from 0 (left) to width of title (right)
     var mouse_y = e.clientY;
     var mouse_x = e.clientX;
-    // example code: sets padding to the x and y position of mouse, pushing the title out and down the page.
+    // example code: sets padding to the x and y position of mouse, pushing the title towards wherever the mouse is
      title.style.paddingTop = String(mouse_y) + "px";
      title.style.paddingLeft = String(mouse_x) + "px";
   });
