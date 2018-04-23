3d63bedd601eef56f995e902e0ec41e9ff1f37d1
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-04-23 03:48:48 -0400

    Update gradienteffect.js

--- gradienteffect.js
+++ gradienteffect.js
@@ -1,6 +1,6 @@
 $(".gradient").mousemove(function( event ) {
   var w = $(this).width(),
-      pct = 360*(+event.pageX)/w,
+      pct = 360*(+event.pageY)/w,
       bg = "linear-gradient(" + pct + "deg,#FF6666,#FF9966)";
       $(".gradient").css("background-image", bg);
 });
