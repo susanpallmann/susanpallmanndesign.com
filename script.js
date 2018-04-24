fb85e5ad4d87de30d342855f3423d34ce93b9f02
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-04-24 17:00:37 +1000

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ window.onload = function() {
   window.addEventListener('scroll', function(e) {
     var scroll_amount = 0;
     $("div.gallery_img").each(function() {
-      alert(($(this).height()*-0.5 * ($(this).offset().top - $(window).scrollTop())/$(window).height()));
+      var scrollAmount = ($(this).height()*-0.5 * ($(this).offset().top - $(window).scrollTop())/$(window).height()));
+      $(this).css("background-position", "0px " + scrollAmount + "px");
     });
     //("background-position", "0px " + ($(this).height()*-0.5 * ($(this).offset().top - $(window).scrollTop())/$(window).height()) + "px");
   });
