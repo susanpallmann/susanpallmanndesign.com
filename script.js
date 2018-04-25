796960e8aef8b7ead18c5a193ae3e59aa7c9c92f
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-04-25 23:36:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -61,14 +61,8 @@ window.onload = function() {
   //body.style.backgroundPosition = "0px " + 0.5*window.scrollY + "px";
   
   // Add event listener for title
-  title.addEventListener("mousemove", function(e) {
-    // mouse_y is the y-value within the title element; ranges from 0 (top) to height of title (left)
-    // mouse_x is the x-value within the title element; ranges from 0 (left) to width of title (right)
-    var mouse_y = e.clientY;
-    var mouse_x = e.clientX;
-    // example code: sets padding to the x and y position of mouse, pushing the title towards wherever the mouse is
-    // title.style.paddingTop = String(mouse_y) + "px";
-    // title.style.paddingLeft = String(mouse_x) + "px";
+  title.addEventListener("mousemove", function() {
+    scrollTo("body");
   });
   
   window.addEventListener("scroll", parallaxScroll);
