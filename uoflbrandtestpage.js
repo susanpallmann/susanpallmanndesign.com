74632a6ff18235e5071a6939df446fcc848add2f
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-08-08 11:11:47 -0400

    Update uoflbrandtestpage.js

--- uoflbrandtestpage.js
+++ uoflbrandtestpage.js
@@ -1,12 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("frm1");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-        text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("formatinfo").innerHTML = text;
-
 function myFunction2() {
     var x = document.getElementById("firstname");
     var text = "";
