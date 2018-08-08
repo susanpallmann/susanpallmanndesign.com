6abe0b92947391941093d80aa6bc47245c38c301
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-08-08 12:38:06 -0400

    Update uoflbrandtestpage.js

--- uoflbrandtestpage.js
+++ uoflbrandtestpage.js
@@ -3,12 +3,13 @@ function myFunction() {
     var lastname = document.getElementById("lastname").value;
     var title = document.getElementById("title").value;
     var department = document.getElementById("department").value;
+    var departmenturl = document.getElementById("departmenturl").value;
     var email = document.getElementById("email").value;
     var officephone = document.getElementById("officephone").value;
     var mobilephone = document.getElementById("mobilephone").value;
     document.getElementById("firstlastname").innerHTML = firstname + " " + lastname;
     document.getElementById("title2").innerHTML = title;
-    document.getElementById("office").innerHTML = department;
+    document.getElementById("office").innerHTML = department.link(departmenturl);
     document.getElementById("ophone").innerHTML = officephone;
     document.getElementById("mphone").innerHTML = mobilephone;
     document.getElementById("mail").innerHTML = email.link("mailto:" + email);
