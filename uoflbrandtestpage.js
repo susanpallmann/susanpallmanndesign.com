3ffbc1b533c5f31a54a09793c86e451ecee51f7b
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-08-08 09:58:54 -0400

    Create uoflbrandtestpage.js

---
+++ uoflbrandtestpage.js
@@ -0,0 +1,11 @@
+<script>
+function myFunction() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("formatinfo").innerHTML = text;
+}
+</script>
