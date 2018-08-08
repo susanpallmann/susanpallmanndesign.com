3929eacc945a667fb10bdf064c3544361fb2080b
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-08-08 11:43:54 -0400

    Update uoflbrandtestpage.js

--- uoflbrandtestpage.js
+++ uoflbrandtestpage.js
@@ -1,5 +1,8 @@
-function myFunction2() {
-    var x = document.getElementById("firstname").value;
+function myFunction() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
     }
-    document.getElementById("firstn").innerHTML = x;
-}
+    document.getElementById("formatinfo").innerHTML = text;
