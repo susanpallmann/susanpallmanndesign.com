35898e810cf92fb0cadbfdb73d43819bee41618f
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-08-08 11:16:12 -0400

    Update uoflbrandtestpage.js

--- uoflbrandtestpage.js
+++ uoflbrandtestpage.js
@@ -1,5 +1,9 @@
 function myFunction2() {
     var x = document.getElementById("firstname");
     var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value;
+    }
     document.getElementById("firstn").innerHTML = text;
 }
