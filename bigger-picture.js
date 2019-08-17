8f5a4a9b6942811cb52a27a38123f5f378cf6f8c
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-08-16 21:23:06 -0400

    Update bigger-picture.js

--- bigger-picture.js
+++ bigger-picture.js
@@ -1,59 +1,59 @@
 pattern = 1;
 window.onload = function() {
-    /* Adjust these global variables */
-    // Speed of hamburger animation (milliseconds). Should match CSS animation speed.
-    hamburgerAnimSpeed = 400;
-    /* End adjustable variables */
-    
-    /* Do not adjust these global variables */
-    scrollValue = -100;
-    screenWidth = screen.width;
-    /* End global variables */
-    
-    $("#top_hamburger").click(function() {
-        if ($(this).hasClass("animcomplete")) {
-            $(this).removeClass("animcomplete");
-            $(this).addClass("closed");
-            $("#dropdown").css("transform", "TranslateY(-200px)");
-            $("#dropdown").css("-webkit-transform", "TranslateY(-200px)");
-            setTimeout(function() {
-              $("#top_hamburger").removeClass("closed");
-            }, hamburgerAnimSpeed);
-        } else {
-            $(this).addClass("open");
-            $("#dropdown").css("transform", "TranslateY(0px)");
-            $("#dropdown").css("-webkit-transform", "TranslateY(0px)");
-            setTimeout(function() {
-              $("#top_hamburger").removeClass("open");
-              $("#top_hamburger").addClass("animcomplete");
-            }, hamburgerAnimSpeed);
-        }
-    });
-    $("#mark").click(function() {
-        if (location.pathname == "/") {
-            scrollToTop();
-        } else {
-            redirect("/");
-        }
-    });
-    $('#sp-icon').mouseleave( function() {
-  if ( pattern < 3 ) {
-    pattern ++;
-  } else {
-    pattern = 1;
-  }
-  switch(pattern) {
-    case 1:
-      $('.pattern-gif').attr('xlink:href', 'images/Pattern 1.svg');
-      break;
-    case 2:
-      $('.pattern-gif').attr('xlink:href', 'images/Pattern 2.svg');
-      break;
-    case 3:
-      $('.pattern-gif').attr('xlink:href', 'images/Pattern 3.svg');
-      break;
-    default:
-      $('.pattern-gif').attr('xlink:href', 'images/Pattern 1');
-  } 
+  /* Adjust these global variables */
+  // Speed of hamburger animation (milliseconds). Should match CSS animation speed.
+  hamburgerAnimSpeed = 400;
+  /* End adjustable variables */
+
+  /* Do not adjust these global variables */
+  scrollValue = -100;
+  screenWidth = screen.width;
+  /* End global variables */
+
+  $("#top_hamburger").click(function() {
+    if ($(this).hasClass("animcomplete")) {
+      $(this).removeClass("animcomplete");
+      $(this).addClass("closed");
+      $("#dropdown").css("transform", "TranslateY(-200px)");
+      $("#dropdown").css("-webkit-transform", "TranslateY(-200px)");
+      setTimeout(function() {
+        $("#top_hamburger").removeClass("closed");
+      }, hamburgerAnimSpeed);
+    } else {
+      $(this).addClass("open");
+      $("#dropdown").css("transform", "TranslateY(0px)");
+      $("#dropdown").css("-webkit-transform", "TranslateY(0px)");
+      setTimeout(function() {
+        $("#top_hamburger").removeClass("open");
+        $("#top_hamburger").addClass("animcomplete");
+      }, hamburgerAnimSpeed);
+    }
+  });
+  $("#mark").click(function() {
+    if (location.pathname == "/") {
+      scrollToTop();
+    } else {
+      redirect("/");
+    }
+  });
+  $('#sp-icon').mouseleave(function() {
+    if (pattern < 3) {
+      pattern++;
+    } else {
+      pattern = 1;
+    }
+    switch (pattern) {
+      case 1:
+        $('.pattern-gif').attr('xlink:href', 'images/Pattern 1.svg');
+        break;
+      case 2:
+        $('.pattern-gif').attr('xlink:href', 'images/Pattern 2.svg');
+        break;
+      case 3:
+        $('.pattern-gif').attr('xlink:href', 'images/Pattern 3.svg');
+        break;
+      default:
+        $('.pattern-gif').attr('xlink:href', 'images/Pattern 1');
+    }
   });
 };
