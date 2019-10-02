c9b59c8e2cfbb6a4b41272bc7f0bd610e15689c4
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-10-02 14:44:27 -0400

    Update bigger-picture.js

--- bigger-picture.js
+++ bigger-picture.js
@@ -57,3 +57,9 @@ window.onload = function() {
     }
   });
 };
+$(".gradient").mousemove(function( event ) {
+  var w = $(this).width(),
+      pct = 360*(+event.pageX)/w,
+      bg = "linear-gradient(" + pct + "deg,#FF6666,#DCA1C4)";
+      $(".gradient").css("background-image", bg);
+});
