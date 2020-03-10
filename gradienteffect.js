1ee780c9d6718ae43bffca86e13eda4c4a2be1e2
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-03-10 11:47:15 -0400

    Delete gradienteffect.js

--- gradienteffect.js
+++
@@ -1,6 +0,0 @@
-$(".gradient").mousemove(function( event ) {
-  var w = $(this).width(),
-      pct = 360*(+event.pageX)/w,
-      bg = "linear-gradient(" + pct + "deg,#FF6666,#FF9966)";
-      $(".gradient").css("background-image", bg);
-});
