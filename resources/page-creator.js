e22b7593161af97cc9d91ef2160c304f0eed9c05
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-14 12:08:55 -0400

    Update page-creator.js

--- resources/page-creator.js
+++ resources/page-creator.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
   $("#page-creator").find("input").change(function(){
     // Get information
+    let color = "#666";
+    let accentColor = "#555";
     if ($('#form-project-color').html() !== '') {
-      let color = $('#form-project-color').html();
-    } else {
-      let color = "#666";
+      color = $('#form-project-color').html();
     }
     if ($('#form-project-accent-color').html() !== '') {
-      let accentColor = $('#form-project-accent-color').html();
-    } else {
-      let accentColor = "#555";
+      accentColor = $('#form-project-accent-color').html();
     }
     // Use information to update our preview panel
     $('.page-hero').css('background-color', color);
