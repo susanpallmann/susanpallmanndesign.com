79de190b2a590f052c185f8cefaae89b22b3da90
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-14 12:05:25 -0400

    Update page-creator.js

--- resources/page-creator.js
+++ resources/page-creator.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     if ($('#form-project-accent-color').val() !== '') {
       let accentColor = $('#form-project-accent-color').val();
     } else {
-      let color = "#555";
+      let accentColor = "#555";
     }
     // Use information to update our preview panel
     $('.page-hero').css('background-color', color);
