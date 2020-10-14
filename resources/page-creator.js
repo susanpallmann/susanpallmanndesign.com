0ce6c83cb54afce05f5735dc6bb14baecaf26cdf
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-14 12:44:30 -0400

    Update page-creator.js

--- resources/page-creator.js
+++ resources/page-creator.js
@@ -3,14 +3,24 @@ $(document).ready(function() {
     // Get information
     let color = "#666";
     let accentColor = "#555";
+    let title = "Project Title";
+    let subtitle = "Project Subtitle";
     if ($('#form-project-color').val() !== '') {
       color = $('#form-project-color').val();
     }
     if ($('#form-project-accent-color').val() !== '') {
       accentColor = $('#form-project-accent-color').val();
     }
+    if ($('#form-project-title').val() !== '') {
+      title = $('#form-project-title').val();
+    }
+    if ($('#form-project-accent-subtitle').val() !== '') {
+      subtitle = $('#form-project-accent-subtitle').val();
+    }
     // Use information to update our preview panel
     $('.page-hero').css('background-color', color);
-    $('.page-accent').css('color', accentColor);
+    $('.page-accent').css('color', accentColor).text(title.toLowerCase());
+    $('.page-subtitle-tiny').text(subtitle);
+    $('.page-title-tiny').text(title);
   }); 
 });
