57da0ee64d9badb78572064b8e030729315b1ff8
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2024-04-12 22:29:13 -0400

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     /* Coding's like riding a bike, right? */
+    $('.ms-grid').mousedown(function(event) {
+    switch (event.which) {
+        case 1:
+            console.log('Left mouse button is pressed');
+            break;
+        case 2:
+            console.log('Middle mouse button is pressed');
+            break;
+        case 3:
+            console.log('Right mouse button is pressed');
+            break;
+        default:
+            console.log('Something went wrong');
+        }
+    });
 });
